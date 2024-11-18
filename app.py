88417766615783279ed5401c2e310f18f2691ec8
shuyomi <nicole030312@naver.com>
2024-11-18 16:00:30 +0900

    페이지구성수정

--- app.py
+++ app.py
@@ -5,31 +5,33 @@ app = Flask(__name__)
 # 첫 화면: index 페이지 라우트
 @app.route('/')
 def index():
-    return render_template('index.html')  # templates/index.html 파일을 렌더링
+    return render_template('index.html')  
+
+# 장소 예약 페이지 라우트
+@app.route('/placereservation')
+def placereservation():
+    return render_template('placereservation.html')  
 
 # 예약 페이지 라우트
 @app.route('/reservation')
 def reservation():
-    return render_template('reservation.html')  # templates/reservation.html 파일을 렌더링
+    return render_template('reservation.html')  
 
 # 좌석 선택 페이지 라우트
 @app.route('/seat_selection')
 def seat_selection():
-    return render_template('seat_selection.html')  # templates/seat_selection.html 파일을 렌더링
-
-# 예약 페이지 라우트
+    return render_template('seat_selection.html')  
+# 예약확인 및 취소 페이지 라우트
 @app.route('/check_cancel')
 def check_cancel():
-    return render_template('check_cancel.html')  # templates/reservation.html 파일을 렌더링
+    return render_template('check_cancel.html') 
 
-# 예약 페이지 라우트
+# 고객센터 페이지 라우트
 @app.route('/support')
 def support():
-    return render_template('support.html')  # templates/reservation.html 파일을 렌더링
+    return render_template('support.html')  
 
 
 # 애플리케이션 실행
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
-
-
