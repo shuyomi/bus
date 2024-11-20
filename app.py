95189b657f793d47266a584b1c59094a295af64c
shuyomi <nicole030312@naver.com>
2024-11-21 04:34:29 +0900

    일정예약 지역선택 페이지 수정

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+# 임시 데이터: 출발지와 도착지
+locations = ["서울", "부산", "대구", "인천", "광주", "대전", "울산", "제주"]
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/placereservation')
+@app.route('/placereservation', methods=['GET', 'POST'])
 def placereservation():
-    return render_template('placereservation.html')
+    if request.method == 'POST':
+        # 사용자가 입력한 출발지와 도착지를 받음
+        departure = request.form['departure']
+        destination = request.form['destination']
+        
+        # 출발지와 도착지를 세션이나 데이터베이스에 저장하거나 다음 페이지로 전달할 수 있음
+        # 여기서는 간단하게 좌석 선택 페이지로 리다이렉트함
+        return redirect(url_for('seat_selection'))
+
+    # GET 요청일 경우, 사용자가 지역 선택 화면을 보도록 함
+    return render_template('placereservation.html', locations=locations)
 
 @app.route('/reservation')
 def reservation():
@@ -26,7 +39,6 @@ def check_cancel():
 def support():
     return render_template('support.html')
 
-# 새로 추가된 운행정보 페이지 라우트
 @app.route('/operation_info')
 def operation_info():
     return render_template('operation_info.html')
